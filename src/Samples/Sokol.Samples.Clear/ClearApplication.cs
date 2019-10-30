e9c701e7a3315d9e606a425b4d65a0cb15decbbc
Lucas Girouard-Stranks <lucas@stranks.ca>
2019-10-29 21:26:15 -0400

    Update clear application

--- src/Samples/Sokol.Samples.Clear/ClearApplication.cs
+++ src/Samples/Sokol.Samples.Clear/ClearApplication.cs
@@ -1,4 +1,4 @@
-using System.Numerics;
+using static SDL2.SDL;
 using static Sokol.sokol_gfx;
 
 namespace Sokol.Samples.Clear
@@ -8,20 +8,23 @@ namespace Sokol.Samples.Clear
         private sg_pass_action _passAction;
 
         public unsafe ClearApplication()
+            : base(true)
         {
             _passAction = new sg_pass_action();
             var colors = _passAction.GetColors();
             colors[0] = new sg_color_attachment_action
             {
-                action = sg_action.SG_ACTION_CLEAR, val = new Vector4(1.0f, 0.0f, 0.0f, 1.0f)
+                action = sg_action.SG_ACTION_CLEAR, 
+                val = RgbaFloat.Red
             };
         }
         
         protected override unsafe void Draw()
         {
-            var g = _passAction.GetColors()[0].val.Y + 0.01f;
-            _passAction.GetColors()[0].val.Y = g > 1.0f ? 0.0f : g;
-            sg_begin_default_pass(ref _passAction, 800, 600);
+            var g = _passAction.GetColors()[0].val.G + 0.01f;
+            _passAction.GetColors()[0].val.G = g > 1.0f ? 0.0f : g;
+            SDL_GL_GetDrawableSize(WindowHandle, out var width, out var height);
+            sg_begin_default_pass(ref _passAction, width, height);
             sg_end_pass();
         }
     }
