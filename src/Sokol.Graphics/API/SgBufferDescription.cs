a30d95ded086bf08ffd98f0d2a7c1116e18b83ec
Lucas Girouard-Stranks <lucas@stranks.ca>
2020-01-10 22:49:36 -0500

    Introduce `SgBufferDescription` for safe API

---
+++ src/Sokol.Graphics/API/SgBufferDescription.cs
@@ -0,0 +1,121 @@
+using System;
+using System.Buffers;
+using System.Runtime.InteropServices;
+using static Sokol.sokol_gfx;
+
+#pragma warning disable 649
+
+namespace Sokol
+{
+    public struct SgBufferDescription
+    {
+        internal sg_buffer_desc desc;
+        
+        private string _name;
+        
+        internal MemoryHandle? DataHandle { get; set; }
+
+        public SgBufferType Type
+        {
+            get
+            {
+                if (desc.type == sg_buffer_type._SG_BUFFERTYPE_DEFAULT)
+                {
+                    desc.type = sg_buffer_type.SG_BUFFERTYPE_VERTEXBUFFER;
+                }
+                return (SgBufferType) (desc.type - 1);
+            }
+            set
+            {
+                // ReSharper disable once ConvertSwitchStatementToSwitchExpression
+                switch (value)
+                {
+                    case SgBufferType.Index:
+                    case SgBufferType.Vertex:
+                        desc.type = (sg_buffer_type) (value + 1);
+                        break;
+                    default:
+                        desc.type = sg_buffer_type._SG_BUFFERTYPE_DEFAULT;
+                        break;
+                }
+            }
+        }
+
+        public SgBufferUsage Usage
+        {
+            get
+            {
+                if (desc.usage == sg_usage._SG_USAGE_DEFAULT)
+                {
+                    desc.usage = sg_usage.SG_USAGE_IMMUTABLE;
+                }
+
+                return (SgBufferUsage) (desc.usage - 1);
+            }
+            set
+            {
+                // ReSharper disable once ConvertSwitchStatementToSwitchExpression
+                switch (value)
+                {
+                    case SgBufferUsage.Immutable:
+                    case SgBufferUsage.Dynamic:
+                    case SgBufferUsage.Stream:
+                        desc.usage = (sg_usage) (value + 1);
+                        break;
+                    default:
+                        desc.usage = sg_usage._SG_USAGE_DEFAULT;
+                        break;
+                }
+            }
+        }
+
+        public int DataSize
+        {
+            get => desc.size;
+            set
+            {
+                if (desc.size <= 0)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), value, null);
+                }
+                
+                desc.size = value;
+            }
+        }
+
+        public unsafe IntPtr DataPointer
+        {
+            get => (IntPtr) desc.content;
+            set => desc.content = (void*) value;
+        }
+
+        public unsafe string Name
+        {
+            get => _name;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    return;
+                }
+                
+                if (!string.IsNullOrEmpty(_name) && desc.label != (void*) 0)
+                {
+                    Marshal.FreeHGlobal((IntPtr) desc.label);
+                }
+
+                desc.label = (byte*) Marshal.StringToHGlobalAnsi(value);
+            }
+        }
+
+        public unsafe void SetData<T>(Memory<T> data) where T : unmanaged
+        {
+            DataSize = Marshal.SizeOf<T>() * data.Length;
+            DataHandle?.Dispose();
+            DataHandle = data.Pin();
+            desc.content = DataHandle.Value.Pointer;
+        }
+
+        internal unsafe IntPtr NameCPointer => (IntPtr) desc.label;
+    }
+}
--- src/Sokol.Graphics/API/SgResource.cs
+++ src/Sokol.Graphics/API/SgResource.cs
@@ -1,80 +1,76 @@
-// /* 
-// MIT License
-//
-// Copyright (c) 2019 Lucas Girouard-Stranks
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-//  */
-//
-// using System;
-// using System.ComponentModel;
-// using System.Runtime.CompilerServices;
-// using System.Runtime.InteropServices;
-// using System.Threading;
-//
-// namespace Sokol
-// {
-//     [EditorBrowsable(EditorBrowsableState.Never)]
-//     public abstract class SgResource : IDisposable
-//     {
-//         private int _disposedState;
-//         
-//         protected readonly IntPtr CNamePointer;
-//
-//         public bool IsDisposed => _disposedState != 0;
-//
-//         public string Name { get; }
-//
-//         public SgResource(string name)
-//         {
-//             Name = name;
-//
-//             if (!string.IsNullOrEmpty(name))
-//             {
-//                 CNamePointer = Marshal.StringToHGlobalAnsi(Name);
-//             }
-//         }
-//
-//         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-//         public void EnsureNotDisposed()
-//         {
-//             if (IsDisposed)
-//             {
-//                 throw new ObjectDisposedException(Name);
-//             }
-//         }
-//
-//         protected virtual void ReleaseUnmanagedResources()
-//         {
-//             Marshal.FreeHGlobal(CNamePointer);
-//         }
-//
-//         public void Dispose()
-//         {
-//             var disposedState = Interlocked.CompareExchange(ref _disposedState, 1, 0);
-//             if (disposedState != 0)
-//             {
-//                 return;
-//             }
-//
-//             ReleaseUnmanagedResources();
-//             GC.SuppressFinalize(this);
-//         }
-//     }
-// }
+/* 
+MIT License
+
+Copyright (c) 2019 Lucas Girouard-Stranks
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Sokol
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public abstract class SgResource : IDisposable
+    {
+        private int _disposedState;
+        
+        protected readonly IntPtr CNamePointer;
+
+        public bool IsDisposed => _disposedState != 0;
+
+        public string Name { get; }
+
+        internal SgResource(string name, IntPtr nameCPointer)
+        {
+            Name = name;
+            CNamePointer = nameCPointer;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void EnsureNotDisposed()
+        {
+            if (IsDisposed)
+            {
+                throw new ObjectDisposedException(Name);
+            }
+        }
+
+        protected virtual void ReleaseUnmanagedResources()
+        {
+            Marshal.FreeHGlobal(CNamePointer);
+        }
+
+        public void Dispose()
+        {
+            var disposedState = Interlocked.CompareExchange(ref _disposedState, 1, 0);
+            if (disposedState != 0)
+            {
+                return;
+            }
+
+            ReleaseUnmanagedResources();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
