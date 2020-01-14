50d7197bd24002706ef67bb5eb4a4f666d27dab4
Lucas Girouard-Stranks <lucas@stranks.ca>
2020-01-14 06:36:24 -0500

    Happy new year!

--- src/Sokol.Graphics/GraphicsBackend.cs
+++ src/Sokol.Graphics/GraphicsBackend.cs
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-Copyright (c) 2019 Lucas Girouard-Stranks
+Copyright (c) 2020 Lucas Girouard-Stranks
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
