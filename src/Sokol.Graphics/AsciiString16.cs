5c9e08a507b157c14520b00a1338b5bfc64d6860
Lucas Girouard-Stranks <lucas@stranks.ca>
2020-01-19 15:44:08 -0500

    Fix `AsciiString16` implicit `IntPtr` cast operator

--- src/Sokol.Graphics/AsciiString16.cs
+++ src/Sokol.Graphics/AsciiString16.cs
@@ -3,26 +3,30 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
 
+// ReSharper disable PrivateFieldCanBeConvertedToLocalVariable
+
 namespace Sokol
 {
+    [StructLayout(LayoutKind.Explicit, Size = 16, Pack = 1)]
     public unsafe struct AsciiString16
     {
-        public fixed byte Data[16];
+        [FieldOffset(0)] private readonly byte Data;
 
         public AsciiString16(string @string)
         {
+            Data = 0;
             var stringLength = @string.Length > 16 ? 16 : @string.Length;
             var stringSpan = @string.AsSpan().Slice(0, stringLength);
-            var ptr = (void*) AsPointer();
+            var ptr = (void*) GetPointer();
             var dataSpan = new Span<byte>(ptr, 16);
             Encoding.ASCII.GetBytes(stringSpan, dataSpan);
         }
 
-        public IntPtr AsPointer()
+        private readonly IntPtr GetPointer()
         {
             fixed (AsciiString16* thisPtr = &this)
             {
-                var ptr = &thisPtr->Data[0];
+                var ptr = &thisPtr->Data;
                 return (IntPtr) ptr;
             }
         }
@@ -32,9 +36,10 @@ namespace Sokol
             return new AsciiString16(@string);
         }
         
-        public static implicit operator IntPtr(AsciiString16 @string)
+        // NOTE: We need this struct to be immutable for the `in` keyword to pass the struct by reference here!
+        public static implicit operator IntPtr(in AsciiString16 @string)
         {
-            return @string.AsPointer();
+            return @string.GetPointer();
         }
     }
 }
