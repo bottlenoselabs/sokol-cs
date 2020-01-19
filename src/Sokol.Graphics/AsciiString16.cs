88d38991ca7e20d5c131c7aa5eb9adf59f3d74c9
Lucas Girouard-Stranks <lucas@stranks.ca>
2020-01-19 15:54:01 -0500

    Drop the explicit struct layout for `AsciiString16`

--- src/Sokol.Graphics/AsciiString16.cs
+++ src/Sokol.Graphics/AsciiString16.cs
@@ -5,16 +5,20 @@ using System.Text;
 
 // ReSharper disable PrivateFieldCanBeConvertedToLocalVariable
 
+#pragma warning disable 414
+
 namespace Sokol
 {
-    [StructLayout(LayoutKind.Explicit, Size = 16, Pack = 1)]
     public unsafe struct AsciiString16
     {
-        [FieldOffset(0)] private readonly byte Data;
+        private readonly int _data0;
+        private readonly int _data1;
+        private readonly int _data2;
+        private readonly int _data3;
 
         public AsciiString16(string @string)
         {
-            Data = 0;
+            _data0 = _data1 = _data2 = _data3 = 0;
             var stringLength = @string.Length > 16 ? 16 : @string.Length;
             var stringSpan = @string.AsSpan().Slice(0, stringLength);
             var ptr = (void*) GetPointer();
@@ -26,7 +30,7 @@ namespace Sokol
         {
             fixed (AsciiString16* thisPtr = &this)
             {
-                var ptr = &thisPtr->Data;
+                var ptr = &thisPtr->_data0;
                 return (IntPtr) ptr;
             }
         }
@@ -37,6 +41,7 @@ namespace Sokol
         }
         
         // NOTE: We need this struct to be immutable for the `in` keyword to pass the struct by reference here!
+        //     If the struct is not immutable (is mutable), this will copy the struct by value (default behaviour)! 
         public static implicit operator IntPtr(in AsciiString16 @string)
         {
             return @string.GetPointer();
