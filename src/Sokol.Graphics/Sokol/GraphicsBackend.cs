f20d4cf04e0e4d8e8844d0f93b93569eef17806e
Lucas Girouard-Stranks <lucas@stranks.ca>
2020-03-17 11:04:53 -0400

    Add `GraphicsBackend`

---
+++ src/Sokol.Graphics/Sokol/GraphicsBackend.cs
@@ -0,0 +1,43 @@
+// Copyright (c) Lucas Girouard-Stranks. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Sokol
+{
+    /// <summary>
+    ///     Specifies the possible graphics API back-ends of Sokol.
+    /// </summary>
+    public enum GraphicsBackend
+    {
+        /// <summary>
+        ///     The OpenGL Core 3.3 back-end. Used for Windows and Linux applications.
+        /// </summary>
+        OpenGL = 0,
+
+        /// <summary>
+        ///     The OpenGL for Embedded Systems 2.0 back-end. Used mostly for Android applications.
+        /// </summary>
+        // ReSharper disable once InconsistentNaming
+        OpenGLES2 = 1,
+
+        /// <summary>
+        ///     The OpenGL for Embedded Systems 3.0 back-end. Used mostly for Android applications.
+        /// </summary>
+        // ReSharper disable once InconsistentNaming
+        OpenGLES3 = 2,
+
+        /// <summary>
+        ///     The Direct3D 11 back-end. Used for desktop applications on Windows.
+        /// </summary>
+        Direct3D11 = 3,
+
+        /// <summary>
+        ///     The Metal back-end. Used for macOS, iOS, and tvOS applications.
+        /// </summary>
+        Metal = 4,
+
+        /// <summary>
+        ///     The dummy back-end. Used for testing and other applications which don't want an implementation of a back-end.
+        /// </summary>
+        Dummy = 5
+    }
+}
