ff74390d7e9a36e58b1477492cc75ba110d134cb
Lucas Girouard-Stranks <lucas@stranks.ca>
2020-04-02 19:41:19 -0400

    Add `BufferType`

---
+++ src/Sokol.Graphics/Sokol.Graphics/BufferType.cs
@@ -0,0 +1,32 @@
+// Copyright (c) Lucas Girouard-Stranks. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Sokol.Graphics
+{
+    /// <summary>
+    ///     Defines the types of data a <see cref="Buffer"/> can contain.
+    /// </summary>
+    /// <remarks>
+    ///     <para>
+    ///         <see cref="BufferType" /> is blittable to the C `sg_buffer_type` enum found in `sokol_gfx`.
+    ///     </para>
+    /// </remarks>
+    public enum BufferType
+    {
+        /// <summary>
+        ///     This value is reserved for the default initialization of structures. The default
+        ///     <see cref="BufferType" /> is <see cref="Vertex" />.
+        /// </summary>
+        Default = 0,
+
+        /// <summary>
+        ///     A <see cref="Buffer"/> contains vertex data.
+        /// </summary>
+        Vertex = 1,
+
+        /// <summary>
+        ///     A <see cref="Buffer"/> contains index data.
+        /// </summary>
+        Index = 2
+    }
+}
