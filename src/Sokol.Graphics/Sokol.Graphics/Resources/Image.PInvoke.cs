c351e2cd2fe4e3418d172a2397790095b04ac96d
Lucas Girouard-Stranks <lucas@stranks.ca>
2020-04-02 19:49:22 -0400

    Add `Image`

---
+++ src/Sokol.Graphics/Sokol.Graphics/Resources/Image.PInvoke.cs
@@ -0,0 +1,51 @@
+// Copyright (c) Lucas Girouard-Stranks. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+namespace Sokol.Graphics
+{
+    public readonly partial struct Image
+    {
+        /// <summary>
+        ///     Fill any zero-initialized members of a <see cref="ImageDescription"/> with their explicit default
+        ///     values.
+        /// </summary>
+        /// <param name="description">The parameters for creating an image.</param>
+        /// <returns>A <see cref="ImageDescription"/> with any zero-initialized members set to default values.</returns>
+        [DllImport(Sg.LibraryName, EntryPoint = "sg_query_image_defaults")]
+        public static extern ImageDescription QueryImageDefaults([In] ref ImageDescription description);
+
+        // TODO: Document allocating an image
+        [SuppressMessage("ReSharper", "SA1600", Justification = "TODO")]
+        [DllImport(Sg.LibraryName, EntryPoint = "sg_alloc_image")]
+        public static extern Image Alloc();
+
+        /// <summary>
+        ///     Creates an <see cref="Image" /> from the specified <see cref="ImageDescription" />.
+        /// </summary>
+        /// <param name="description">The parameters for creating an image.</param>
+        /// <returns>A <see cref="Buffer" />.</returns>
+        [DllImport(Sg.LibraryName, EntryPoint = "sg_make_image")]
+        public static extern Image Create([In] ref ImageDescription description);
+
+        [DllImport(Sg.LibraryName, EntryPoint = "sg_destroy_image")]
+        private static extern void Destroy(Image image);
+
+        [DllImport(Sg.LibraryName, EntryPoint = "sg_init_image")]
+        private static extern void Init(Image image, [In] ref ImageDescription description);
+
+        [DllImport(Sg.LibraryName, EntryPoint = "sg_fail_image")]
+        private static extern void Fail(Image image);
+
+        [DllImport(Sg.LibraryName, EntryPoint = "sg_update_image")]
+        private static extern void Update(Image image, ref ImageContent data);
+
+        [DllImport(Sg.LibraryName, EntryPoint = "sg_query_image_state")]
+        private static extern ResourceState QueryState(Image image);
+
+        [DllImport(Sg.LibraryName, EntryPoint = "sg_query_image_info")]
+        private static extern ImageInfo QueryInfo(Image image);
+    }
+}
---
+++ src/Sokol.Graphics/Sokol.Graphics/Resources/Image.cs
@@ -0,0 +1,97 @@
+// Copyright (c) Lucas Girouard-Stranks. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+// ReSharper disable UnusedType.Global
+// ReSharper disable FieldCanBeMadeReadOnly.Global
+// ReSharper disable MemberCanBePrivate.Global
+namespace Sokol.Graphics
+{
+    /// <summary>
+    ///     A graphics resource that holds one or many layers of structured texture elements, called texels, and the
+    ///     related information such as how many elements there are and how they are encoded and organized.
+    /// </summary>
+    /// <remarks>
+    ///     <para>
+    ///         An <see cref="Image" /> is either used as the input to a <see cref="Shader" /> or as the output of a
+    ///         <see cref="Pass" />. When an <see cref="Image" /> is used as input, it is often referred to as a
+    ///         "texture". When it is used as an output it is often referred to as a "render target". Underlying APIs,
+    ///         such as OpenGL, will mean "texture" in general for an <see cref="Image" /> regardless if it is being
+    ///         referred to as input or output.
+    ///     </para>
+    ///     <para>
+    ///         An <see cref="Image" /> may have multiple layers called "sub images" which are most often used as
+    ///         mipmaps. See <a cref="http://en.wikipedia.org/wiki/Mipmap">https://en.wikipedia.org/wiki/Mipmap</a>
+    ///         for more information.
+    ///     </para>
+    ///     <para>
+    ///         The format, encoding, and organization of an <see cref="Image" /> never change. However, you can change
+    ///         the contents either by rendering to it or copying data into it.
+    ///     </para>
+    ///     <para>
+    ///         A <see cref="Image" /> must only be used or destroyed with the same active <see cref="Context" /> that
+    ///         was also active when the resource was created.
+    ///     </para>
+    ///     <para>
+    ///         <see cref="Image" /> is blittable to the C `sg_image` struct found in `sokol_gfx`.
+    ///     </para>
+    /// </remarks>
+    [StructLayout(LayoutKind.Explicit, Size = 4, Pack = 4)]
+    public readonly partial struct Image
+    {
+        /// <summary>
+        ///     A number which uniquely identifies the <see cref="Image" />.
+        /// </summary>
+        [FieldOffset(0)]
+        public readonly uint Identifier;
+
+        // TODO: Document `BufferInfo`.
+        [SuppressMessage("ReSharper", "SA1600", Justification = "TODO")]
+        public ImageInfo Info => QueryInfo(this);
+
+        // TODO: Document `ResourceState`.
+        [SuppressMessage("ReSharper", "SA1600", Justification = "TODO")]
+        public ResourceState State => QueryState(this);
+
+        /// <summary>
+        ///     Destroys the <see cref="Image" />.
+        /// </summary>
+        public void Destroy()
+        {
+            Destroy(this);
+        }
+
+        // TODO: Document manual initialization of an image.
+        [SuppressMessage("ReSharper", "SA1600", Justification = "TODO")]
+        public void Init([In] ref ImageDescription description)
+        {
+            Init(this, ref description);
+        }
+
+        // TODO: Document failing an image.
+        [SuppressMessage("ReSharper", "SA1600", Justification = "TODO")]
+        public void Fail()
+        {
+            Fail(this);
+        }
+
+        /// <summary>
+        ///     Overwrites the contents of the <see cref="Image" /> by copying any pointed memory in the specified
+        ///     <see cref="ImageContent" />. The <see cref="Image" /> must have been created
+        ///     using <see cref="ResourceUsage.Dynamic" /> or <see cref="ResourceUsage.Stream" />.
+        /// </summary>
+        /// <param name="data">The new contents of the image.</param>
+        public void Update(ref ImageContent data)
+        {
+            Update(this, ref data);
+        }
+
+        /// <inheritdoc />
+        public override string ToString()
+        {
+            return $"{Identifier}";
+        }
+    }
+}
