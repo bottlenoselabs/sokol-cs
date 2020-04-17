ac39f4cd59f954befd2870bed00f282c69043131
Lucas Girouard-Stranks <lucas@stranks.ca>
2020-04-17 00:08:24 -0400

    Add helper `SetData` method to `ImageSubContent`

--- src/Sokol.Graphics/Sokol.Graphics/Resources/Image/ImageSubContent.cs
+++ src/Sokol.Graphics/Sokol.Graphics/Resources/Image/ImageSubContent.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 namespace Sokol.Graphics
@@ -16,12 +17,37 @@ namespace Sokol.Graphics
         ///     The pointer to the starting address of the block of bytes as data.
         /// </summary>
         [FieldOffset(0)]
-        public IntPtr Pointer;
+        public IntPtr Data;
 
         /// <summary>
         ///     The size of the block of bytes as data.
         /// </summary>
         [FieldOffset(8)]
         public int Size;
+
+        /// <summary>
+        ///     Sets the <see cref="Data" /> and <see cref="Size" /> fields of the <see cref="ImageSubContent" /> given
+        ///     the specified <see cref="Span{T}" /> struct. It is assumed that the <paramref name="data" /> is
+        ///     already unmanaged or externally pinned.
+        /// </summary>
+        /// <param name="data">The memory block.</param>
+        /// <typeparam name="T">The type of data in the memory block.</typeparam>
+        public unsafe void SetData<T>(Span<T> data)
+        {
+            ref var reference = ref MemoryMarshal.GetReference(data);
+            Data = (IntPtr)Unsafe.AsPointer(ref reference);
+            Size = Marshal.SizeOf<T>() * data.Length;
+        }
+
+        /// <summary>
+        ///     Sets the <see cref="Data" /> and <see cref="Size" /> fields of the <see cref="ImageSubContent" /> given
+        ///     the specified <see cref="Memory{T}" /> struct.
+        /// </summary>
+        /// <param name="data">The memory block.</param>
+        /// <typeparam name="T">The type of data in the memory block.</typeparam>
+        public void SetData<T>(Memory<T> data)
+        {
+            SetData(data.Span);
+        }
     }
 }
