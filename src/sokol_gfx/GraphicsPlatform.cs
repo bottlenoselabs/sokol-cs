d80c988c6f0aab278867c4ede9384ce2b1d8c78c
Lucas Girouard-Stranks <lucas@stranks.ca>
2020-06-04 16:51:19 -0400

    Add Android and iOS to `GraphicsPlatform` enum

--- src/sokol_gfx/GraphicsPlatform.cs
+++ src/sokol_gfx/GraphicsPlatform.cs
@@ -23,6 +23,7 @@ using System.Diagnostics.CodeAnalysis;
 ///     </para>
 /// </remarks>
 [Flags]
+[SuppressMessage("ReSharper", "MemberCanBeInternal", Justification = "Public API.")]
 public enum GraphicsPlatform
 {
     /// <summary>
@@ -31,21 +32,35 @@ public enum GraphicsPlatform
     Unknown = 0,
 
     /// <summary>
-    ///     Desktop versions of Windows on 64-bit computing architecture (e.g. Windows 7, Windows 8.1, Windows 10, and up).
+    ///     Desktop versions of Windows on 64-bit computing architecture. Includes Windows 7, Windows 8.1, Windows 10,
+    ///     and up.
     /// </summary>
     Windows = 1 << 0,
 
     /// <summary>
-    ///     Desktop versions of OSX on 64-bit computing architecture (e.g. macOS 10.3 and up).
+    ///     Desktop versions of OSX on 64-bit computing architecture. Includes macOS 10.3 and up.
     /// </summary>
-    [SuppressMessage("ReSharper", "InconsistentNaming", Justification = "Product name")]
-    [SuppressMessage("ReSharper", "SA1300", Justification = "Product name")]
+    [SuppressMessage("ReSharper", "InconsistentNaming", Justification = "Product name.")]
+    [SuppressMessage("ReSharper", "SA1300", Justification = "Product name.")]
     macOS = 1 << 1,
 
     /// <summary>
-    ///     Desktop distributions of Linux on 64-bit computing architecture (e.g. CentOS, Debian, Fedora, Ubuntu, etc)
+    ///     Desktop distributions of Linux on 64-bit computing architecture. Includes, but is not limited to, CentOS,
+    ///     Debian, Fedora, and Ubuntu.
     /// </summary>
-    Linux = 1 << 2
+    Linux = 1 << 2,
 
-    // TODO: Android, iOS, tvOS, RaspberryPi, WebAssembly, PlayStation4, PlayStationVita, Switch etc
+    /// <summary>
+    ///     Mobile versions of Android on 64-bit computing architecture. Includes Android 5.x and up.
+    /// </summary>
+    Android = 1 << 3,
+
+    /// <summary>
+    ///     Mobile versions of iOS on 64-bit computing architecture. Includes iOS 11.x and up.
+    /// </summary>
+    [SuppressMessage("ReSharper", "InconsistentNaming", Justification = "Product name.")]
+    [SuppressMessage("ReSharper", "SA1300", Justification = "Product name.")]
+    iOS = 1 << 4,
+
+    // TODO: tvOS, RaspberryPi, WebAssembly, PlayStation4, PlayStationVita, Switch etc
 }
