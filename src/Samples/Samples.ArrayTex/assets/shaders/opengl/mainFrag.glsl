8b95eafb0fdaded1748aadc980189f3cb9fd6763
Lucas Girouard-Stranks <lucas@stranks.ca>
2020-06-04 18:39:19 -0400

    Update native libraries for Direct3D11

--- src/Samples/Samples.ArrayTex/assets/shaders/opengl/mainFrag.glsl
+++ src/Samples/Samples.ArrayTex/assets/shaders/opengl/mainFrag.glsl
@@ -3,12 +3,11 @@ uniform sampler2DArray tex;
 in vec3 uv0;
 in vec3 uv1;
 in vec3 uv2;
-in vec3 cweights;
 out vec4 frag_color;
 void main()
 {
     vec4 c0 = texture(tex, uv0);
     vec4 c1 = texture(tex, uv1);
     vec4 c2 = texture(tex, uv2);
-    frag_color = vec4(c0.xyz * cweights.x + c1.xyz * cweights.y + c2.xyz * cweights.z, 1.0);
+    frag_color = vec4(c0.xyz + c1.xyz + c2.xyz, 1.0);
 }
