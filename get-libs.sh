7632546aa643941209e5575499de458c69921bdc
Lucas Girouard-Stranks <lucas.stranks@nventive.com>
2021-01-01 00:37:58 -0400

    Fix get-libs.sh for Windows WSL

--- get-libs.sh
+++ get-libs.sh
@@ -6,17 +6,17 @@ LIB_DIR=$MY_DIR/lib
 
  # Downloading
 echo "Downloading latest sokol libraries ..."
-curl -OL https://github.com/lithiumtoast/sokol-bin/releases/latest/download/win-x64.zip > "$MY_DIR/win-x64.zip"
+curl -OL https://github.com/lithiumtoast/sokol-bin/releases/latest/download/win-x64.zip --compressed > "$MY_DIR/win-x64.zip"
 if [ $? -ne 0 ]; then
     >&2 echo "ERROR: Unable to download successfully."
     exit 1
 fi
-curl -OL https://github.com/lithiumtoast/sokol-bin/releases/latest/download/osx-x64.zip > "$MY_DIR/osx-x64.zip"
+curl -OL https://github.com/lithiumtoast/sokol-bin/releases/latest/download/osx-x64.zip --compressed > "$MY_DIR/osx-x64.zip"
 if [ $? -ne 0 ]; then
     >&2 echo "ERROR: Unable to download successfully."
     exit 1
 fi
-curl -OL https://github.com/lithiumtoast/sokol-bin/releases/latest/download/linux-x64.zip > "$MY_DIR/linux-x64.zip"
+curl -OL https://github.com/lithiumtoast/sokol-bin/releases/latest/download/linux-x64.zip --compressed > "$MY_DIR/linux-x64.zip"
 if [ $? -ne 0 ]; then
     >&2 echo "ERROR: Unable to download successfully."
     exit 1
@@ -28,9 +28,9 @@ echo "Finished downloading!"
 echo "Decompressing libraries ..."
 rm -rf $LIB_DIR/libsokol
 mkdir -p $LIB_DIR/libsokol
-tar xC $LIB_DIR/libsokol -f $MY_DIR/win-x64.zip
-tar xC $LIB_DIR/libsokol -f $MY_DIR/osx-x64.zip
-tar xC $LIB_DIR/libsokol -f $MY_DIR/linux-x64.zip
+unzip $MY_DIR/win-x64.zip -d $LIB_DIR/libsokol 
+unzip $MY_DIR/osx-x64.zip -d $LIB_DIR/libsokol 
+unzip $MY_DIR/linux-x64.zip -d $LIB_DIR/libsokol 
 if [ $? -ne 0 ]; then
     >&2 echo "ERROR: Unable to decompress successfully."
     exit 1
