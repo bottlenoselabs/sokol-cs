b5a432352a9a3e243fdd583aec6259ef629fac67
Lucas Girouard-Stranks <lustranks@gmail.com>
2022-06-17 14:32:37 -0400

    Update `library.sh`

--- library.sh
+++ library.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-$DIR/ext/scripts/c/library/main.sh $DIR/src/c/sokol $DIR/lib sokol "" "" ""
+$DIR/ext/scripts/c/library/main.sh \
+    $DIR/src/c/sokol \
+    $DIR/lib sokol
