f8d8bcb8e29afe81080d00101e4ee205d5c9280c
Lucas Girouard-Stranks <lustranks@gmail.com>
2022-06-26 21:59:02 -0400

    Fix for updated scripts repo dependency

--- library.sh
+++ library.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 $DIR/ext/scripts/c/library/main.sh \
     $DIR/src/c/sokol \
-    $DIR/lib sokol
+    $DIR/lib \
+    "sokol" \
+    "sokol"
