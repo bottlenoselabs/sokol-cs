a2bfb9214818d89c2c008ad8510b44bf0b3bef20
Lucas Girouard-Stranks <lithiumtoast@users.noreply.github.com>
2022-10-21 13:03:58 -0400

    Update `library.sh`

--- library.sh
+++ library.sh
@@ -3,6 +3,13 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 $DIR/ext/scripts/c/library/main.sh \
     $DIR/src/c/sokol \
+    $DIRECTORY/build \
     $DIR/lib \
     "sokol" \
-    "sokol"
+    "sokol" \
+    "" \
+    ""
+
+if [[ -z "$1" ]]; then
+    read
+fi
