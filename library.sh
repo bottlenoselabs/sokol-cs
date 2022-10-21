adfe473b4d7f8da561cfb59edd29733059ef7c50
Lucas Girouard-Stranks <lithiumtoast@users.noreply.github.com>
2022-10-21 13:19:46 -0400

    Update `library.sh`

--- library.sh
+++ library.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-$DIR/ext/scripts/c/library/main.sh \
-    $DIR/src/c/sokol \
+$DIRECTORY/ext/scripts/c/library/main.sh \
+    $DIRECTORY/src/c/sokol \
     $DIRECTORY/build \
-    $DIR/lib \
+    $DIRECTORY/lib \
     "sokol" \
     "sokol" \
     "" \
